40e18047588abbdac8f53386b38df97e40be921d
Kaimann <32804771+Kaimann@users.noreply.github.com>
2018-09-05 19:58:10 +0300

    112

--- src/main/java/Main2.java
+++
@@ -1,9 +0,0 @@
-public class Main2 {
-    public static void main(String[] args) {
-        System.out.println("Привет всем!!!");
-        int n = 9;
-        n++;
-        System.out.println(n);
-        System.out.println(n+n);
-    }
-}
